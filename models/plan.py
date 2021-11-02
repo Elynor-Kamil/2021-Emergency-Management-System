263aeb46ec2c5b57e7a6e11ce4186c3a2407a5c4
Dennis Yung <dennisylyung@gmail.com>
2021-11-02 22:14:35 +0000

    basic plan implementation

--- models/plan.py
+++ models/plan.py
@@ -1,2 +1,36 @@
+from collections import Iterable
+from datetime import date
+from enum import Enum
+
+from models.camp import Camp
+
+
 class Plan:
-    pass
+    class EmergencyType(Enum):
+        EARTHQUAKE = 'earthquake'
+        FIRE = 'fire'
+        TSUNAMI = 'tsunami'
+        STORM = 'storm'
+
+    def __init__(self,
+                 name: str,
+                 emergency_type: EmergencyType,
+                 description: str,
+                 geographical_area: str,
+                 start_date: date,
+                 camps: Iterable[Camp] = None):
+        self.name = name
+        self.emergency_type = emergency_type
+        self.description = description
+        self.geographical_area = geographical_area
+        self.start_date = start_date
+        self.camps: set[Camp] = set(camps) or set()
+
+    def __str__(self):
+        return f'Plan "{self.name}"'
+
+    def open_camps(self, *camps):
+        self.camps.update(*camps)
+
+    def close_camps(self, *camps):
+        self.camps.difference_update(*camps)
