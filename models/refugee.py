d59a818c8a63dcf975c95b755d3a321fd2fb11a3
Michelle Chan <michelle.112348@gmail.com>
2021-11-03 16:47:32 +0000

    add Refugee functions

--- models/refugee.py
+++ models/refugee.py
@@ -1,6 +1,23 @@
 class Refugee:
-    def __init__(self):
-        self.family = family
+    def __init__(self, familyName, familyMember):
+        self.familyName = familyName
+        self.familyMember = familyMember
+
+    def findFamilyMember(self):
+        return self.familyMember
+
+    def changeFamilyMember(self, familyMemberName):
+        self.familyMember()
 
     def __str__(self):
         return self.family
+
+family = Refugee("Chan", 3)
+
+print(family.findFamilyMember())
+
+
+
+
+
+    
