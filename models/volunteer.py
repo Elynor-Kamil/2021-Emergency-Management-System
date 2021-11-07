43ea62b1460a47351a23cc344b46d19243e3245c
vwcchan <zczlwc1@ucl.ac.uk>
2021-11-07 16:36:46 +0000

    add edit features in class Volunteer

--- models/volunteer.py
+++ models/volunteer.py
@@ -2,13 +2,35 @@ import datetime
 
 class Volunteer:
     """
+    A class used to represent a volunteer.
 
+    Attributes
+    ----------
+    name : str
+        the name of the volunteer
+    phone : str
+        the phone number of the volunteer
+    volunteercamp : string
+        the camp that the volunteer belongs to
+    availability: boolean
+        whether the is available to be involved in a new plan
+    creationdate : date
+        the date when the volunteer account is created
+
+
+    Methods
+    -------
+    changeVolunteerName(newName)
+        change the name of the volunteer and check if the new name is valid
+    changeVolunteerName(newPhone)
+        change the phone number of the volunteer
     """
+
     def __init__(self,
                  name: str,
                  phone: str,
-                 volunteercamp: str,
-                 availability: bool):
+                 volunteercamp: list,
+                 availability = True):
         currentdate = datetime.datetime.now()
 
 
@@ -20,19 +42,35 @@ class Volunteer:
 
 
     def changeVolunteerName(self, newName):
-        self.name = newName
+        if len(newName) > 1:
+            self.name = newName
+        else:
+            pass #ask to re-enter valid name
 
     def changeVolunteerPhone(self,newPhone):
         self.phone = newPhone
 
-    def changeAvailability(self, newStatus):
-        self.availability = newStatus
+    def changeAvailability(self):
+        self.availability = False
 
+    def changeCamp(self, newCamp):
+        self.camp = newCamp
+        #if newCamp not in self.camp:
+        #    self.camp.append(newCamp)
+
+    def removeCamp(self, camp):
+        if camp in self.camp:
+            self.camp.remove(camp)
 
     def __str__(self):
-        return f"Volunteer {self.name} belongs to camp {self.camp}. Joined date: {self.creationdate}"
+        return f"Volunteer {self.name} belongs to camp {self.camp}.\n" \
+               f"Phone Number: {self.phone}\n" \
+               f"Availability: {self.availability}\n" \
+               f"Joined date: {self.creationdate}\n"
 
 if __name__ == "__main__":
 
-    volunteerA = Volunteer(name="Vanessa", phone="+4477123456", volunteercamp="UCL", availability=True)
+    volunteerA = Volunteer(name="Vanessa", phone="+4477123456", volunteercamp="UCL")
+    print(volunteerA)
+    volunteerA.changeCamp("UCL2")
     print(volunteerA)
