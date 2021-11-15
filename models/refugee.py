fd4322b84b7e5bc7e9b07762ad09c825afb8b2fd
Michelle Chan <michelle.112348@gmail.com>
2021-11-15 14:40:33 +0000

    update refugee class

--- models/refugee.py
+++ models/refugee.py
@@ -6,26 +6,26 @@ class Refugee:
     """
     def __init__(self,
                  name: str,
-                 identificationOfCamp: str,
+                 camp: str,
                  medicalCondition: str,
                  numOfFamilyMember: int,
-                 dateOfClosing: date):
+                 dateOfClosing=None):
 
         """
         Initialise a new refugee.
         :param name of refugee family
         :param number of family member
-        :param identification of campus the refugee family locating
+        :param camp of the refugee family locating at
         :param medical condition of the refugee
-        :param start date of refugee family creation
+        :param start date of refugee family
         :param closing date of refugee family
         """
         self.name = name
         self.numOfFamilyMember = numOfFamilyMember + 1
-        self.identificationOfCamp = identificationOfCamp
+        self.camp =camp
         self.medicalCondition = medicalCondition
         self.dateOfCreation = datetime.today().date()
-        self.dateOfClosing = dateOfClosing #raise InvalidDateError if closing date has past
+        self.dateOfClosing = dateOfClosing
 
     def formatName(self, name):
         """
@@ -38,14 +38,17 @@ class Refugee:
         """
         format number of family member
         """
-        if numOfFamilyMember > 1:
-            self.numOfFamilyMember = numOfFamilyMember + 1
+        if numOfFamilyMember is not int or numOfFamilyMember < 0:
+            raise self.InvalidNumIfFamilyMemberException()
+        self.numOfFamilyMember = numOfFamilyMember + 1
 
-    def formatIdentificationOfCamp(self, identificationOfCamp):
+    def formatCamp(self, newCamp):
         """
         format identification camp of refugee
         """
-        self.identificationOfCamp = identificationOfCamp
+        if newCamp not in camp.Camp.camps:
+            raise self.InvalidCampException(newCamp)
+        self.camp = newCamp
 
     def formatMedicalCondition(self, medicalCondition): #medical condition types provided or a description?
         """
@@ -58,17 +61,47 @@ class Refugee:
         format closing date of refugee family
         """
         today = datetime.today().date()
-        if today > dateOfClosing:
-            raise self.InvalidDateException()
-        return dateOfClosing
+        if today >= dateOfClosing:
+            raise self.InvalidClosingDateException()
+        self.dateOfClosing = dateOfClosing
 
 
     def __str__(self):
-        return f"Refugee family {self.name} located in {self.identificationOfCamp}.\n"\
+        return f"Refugee family {self.name} located in {self.camp}.\n"\
                f"number of family member: {self.numOfFamilyMember}\n" \
                f"medical condition: {self.medicalCondition}\n" \
                f"creation date: {self.dateOfCreation}\n" \
                f"closing date: {self.dateOfClosing}\n"
+
+
+    class InvalidNumOfFamilyMemberException(Exception):
+        """
+        Raise exception when number of family member entered.
+        """
+        def __init__(self):
+            super().__init__(f"Invalid number of family member. Family member must be an positive integer.")
+
+
+    class InvalidCampException(Exception):
+        """
+        Raise exception when camp entered does not exist.
+        """
+
+        def __init__(self, camp):
+            super().__init__(f"Camp {camp} does not exist.")
+
+
+    class InvalidClosingDateException(Exception):
+        """
+        Raise exception when the close date is invalid.
+        """
+        def __init__(self):
+            super().__init__(f"Invalid closing date. Closing date must be after current date.")
+
+
+
+
+
 
 
 
@@ -76,5 +109,6 @@ class Refugee:
 r = Refugee("Chan", "London", "severe", 3, date(2022,3,4))
 r.formatDateOfClosing(date(2020, 4, 5))
 print(r.__str__())
+
 
 
