2353e9bce0a978d9b28afb8247be7d9ef217eb59
Dennis Yung <dennisylyung@gmail.com>
2021-11-17 18:23:16 +0000

    refactoring for clarity

--- models/tests/base/test_document.py
+++ models/tests/base/test_document.py
@@ -1,70 +1,70 @@
 from unittest import TestCase
 
-from models.base.document import Document, EmbeddedDocument
+from models.base.document import IndexedDocument, Document
 from models.base.field import Field, ReferenceDocumentSetField
 
 
 class MetaDocumentTest(TestCase):
     def test_multiple_primary_key(self):
         with self.assertRaises(Field.MultiplePrimaryKeyError):
-            class DemoDocument(Document):
+            class DemoIndexedDocument(IndexedDocument):
                 id = Field(primary_key=True)
                 name = Field(primary_key=True)
 
 
 class BasicDocumentTest(TestCase):
-    class DemoDocument(Document):
+    class DemoIndexedDocument(IndexedDocument):
         name = Field(primary_key=True)
         password = Field()
 
     def test_create_document(self):
-        doc = self.DemoDocument(name='test', password='test')
+        doc = self.DemoIndexedDocument(name='test', password='test')
         self.assertEqual(doc.name, 'test')
         self.assertEqual(doc.password, 'test')
 
     def test_compulsory_primary_key(self):
         with self.assertRaises(Field.PrimaryKeyNotSetError):
-            self.DemoDocument(password='test')
+            self.DemoIndexedDocument(password='test')
 
 
 class DocumentPersistenceTest(TestCase):
-    class DemoDocument(Document):
+    class DemoIndexedDocument(IndexedDocument):
         id = Field(primary_key=True)
         name = Field()
 
     def setUp(self) -> None:
-        self.DemoDocument(id=1, name='a')
-        self.DemoDocument(id=2, name='b')
-        self.DemoDocument(id=3, name='c')
+        self.DemoIndexedDocument(id=1, name='a')
+        self.DemoIndexedDocument(id=2, name='b')
+        self.DemoIndexedDocument(id=3, name='c')
 
     def test_persistence(self):
-        self.DemoDocument.reload()
-        self.assertEqual(len(self.DemoDocument.all()), 3)
+        self.DemoIndexedDocument.reload()
+        self.assertEqual(len(self.DemoIndexedDocument.all()), 3)
 
     def test_find(self):
-        document = self.DemoDocument.find(1)
+        document = self.DemoIndexedDocument.find(1)
         self.assertEqual(document.name, 'a')
-        document = self.DemoDocument.find(10)
+        document = self.DemoIndexedDocument.find(10)
         self.assertIsNone(document)
 
     def test_delete(self):
-        self.DemoDocument(id=4, name='d')
-        document = self.DemoDocument.find(4)
+        self.DemoIndexedDocument(id=4, name='d')
+        document = self.DemoIndexedDocument.find(4)
         self.assertIsNotNone(document)
         document.delete()
-        self.DemoDocument.reload()
-        document = self.DemoDocument.find(4)
+        self.DemoIndexedDocument.reload()
+        document = self.DemoIndexedDocument.find(4)
         self.assertIsNone(document)
 
     def tearDown(self) -> None:
-        self.DemoDocument.delete_all()
+        self.DemoIndexedDocument.delete_all()
 
 
 class EmbeddedDocumentTest(TestCase):
-    class DemoEmbeddedDocument(EmbeddedDocument):
+    class DemoEmbeddedDocument(Document):
         name = Field()
 
-    class DemoNestedDocument(Document):
+    class DemoNestedDocument(IndexedDocument):
         name = Field(primary_key=True)
         children = ReferenceDocumentSetField()
 
