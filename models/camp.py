fe36aaff5df90046b502cd51302386b46cb955f8
Dennis Yung <dennisylyung@gmail.com>
2021-11-18 12:52:17 +0000

    adapt plan class to IndexedDocument

--- models/camp.py
+++ models/camp.py
@@ -1,6 +1,9 @@
-class Camp:
-    def __init__(self, name: str):
-        self.name = name
+from models.base.document import Document
+from models.base.field import Field
+
+
+class Camp(Document):
+    name = Field(primary_key=True)
 
     def __str__(self):
         return self.name
