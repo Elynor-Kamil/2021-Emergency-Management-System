5a4c4af55a35411cc4341b9eb02419c89558f781
Michelle Chan <michelle.112348@gmail.com>
2021-11-20 10:41:55 +0000

    restore camp class

--- models/camp.py
+++ models/camp.py
@@ -1,11 +1,6 @@
-from models.refugee import Refugee
-from models.Volunteer import Volunteer
-
 class Camp:
     def __init__(self, name: str):
         self.name = name
-        self.refugees = set(Refugee) #Record refugees
-        self.volunteers = set(Volunteer) #Record volunteers
 
     def __str__(self):
         return self.name
