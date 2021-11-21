a9d0ab19055a9adc4f7a53b39e592a3d619de578
Dora <dorayin0602@gmail.com>
2021-11-21 12:15:59 +0000

    Saved volunteer2.py and test_volunteer2.py for release 2

--- models/volunteer2.py
+++ models/volunteer2.py
@@ -1,5 +1,4 @@
-import datetime
-from models import camp
+import datetime, re
 from models.base.document import IndexedDocument
 from models.base.field import Field
 from models.camp import Camp
@@ -8,12 +7,13 @@ from models.camp import Camp
 class Volunteer(IndexedDocument):
     username = Field(primary_key=True)
     password = Field()
+    accountActivated = Field()
     firstname = Field()
     lastname = Field()
     phone = Field()
     camp = Field()
     availability = Field()
-    creationdate = Field()
+    creationDate = Field()
 
     def __init__(self,
                  username: str,
@@ -22,18 +22,20 @@ class Volunteer(IndexedDocument):
                  lastname: str,
                  phone: str,
                  camp: Camp,
+                 accountActivated=True,
                  availability=True):
 
         """
         Initialize a new volunteer account.
         :param username: username of the volunteer account
         :param password: password of the volunteer account
+        :param accountActivated: activation status of the volunteer account
         :param firstname: the firstname of the volunteer
         :param lastname: the lastname of the volunteer
         :param phone: the phone number of the volunteer (only numbers with international code are accepted)
         :param camp: the camp that the volunteer belongs to
         :param availability:  whether the volunteer is available to join a new emergency plan
-        :param creationdate: the date that this volunteer is created
+        :param creationDate: the date that this volunteer is created
         """
 
         self.__checkVolunteerUsername(username)
@@ -43,19 +45,20 @@ class Volunteer(IndexedDocument):
 
         super().__init__(username=username,
                          password=password,
+                         accountActivated=accountActivated,
                          firstname=firstname,
                          lastname=lastname,
                          phone=phone,
                          camp=camp,
                          availability=availability,
-                         creationdate=datetime.datetime.now().date())
+                         creationDate=datetime.datetime.now().date())
 
     def __checkVolunteerUsername(self, username):
-        if len(username) <= 1:
+        if len(username) < 4:
             raise self.InvalidUsernameException(username)
 
     def __checkVolunteerPassword(self, password):
-        if len(password) <= 1:
+        if len(password) < 4:
             raise self.InvalidPasswordException(password)
 
     def __checkVolunteerName(self, firstname, lastname):
@@ -65,36 +68,33 @@ class Volunteer(IndexedDocument):
             raise self.InvalidLastnameException(lastname)
 
     def __checkVolunteerPhone(self, phone):
-        if phone[0] != "+":
-            raise self.InvalidPhoneException("Phone should include country code and be in format \"+XXXXXXXXXXX\".")
-        elif len(phone) < 5:
-            raise self.InvalidPhoneException("The phone number is too short.")
-
-    def changeAvailability(self):
-        self.availability = not (self.availability)
+        phoneText = "\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{5,14}$"
+        if not re.search(phoneText, phone):
+            raise self.InvalidPhoneException(phone)
 
     def __str__(self):
         return f"Volunteer username: {self.username}\n" \
+               f"Account activated: {self.accountActivated}\n" \
                f"Volunteer {self.firstname} {self.lastname} belongs to camp {self.camp}.\n" \
                f"Phone Number: {self.phone}\n" \
                f"Availability: {self.availability}\n" \
-               f"Date joined: {self.creationdate}\n"
+               f"Date joined: {self.creationDate}\n"
 
     class InvalidUsernameException(Exception):
         """
-         Raise exception when the username entered is too short.
+         Raise exception when the username entered is less than 4 characters.
         """
 
         def __init__(self, username):
-            super().__init__(f"Invalid username: {username}. Username should be more than 1 character.")
+            super().__init__(f"Invalid username: {username}. Username should be at least 4 characters.")
 
     class InvalidPasswordException(Exception):
         """
-         Raise exception when the password entered is too short.
+         Raise exception when the password entered is less than 4 characters.
         """
 
         def __init__(self, password):
-            super().__init__(f"Invalid password: {password}. Password should be more than 1 character.")
+            super().__init__(f"Invalid password: {password}. Password should be at least 4 characters.")
 
     class InvalidFirstnameException(Exception):
         """
@@ -117,8 +117,8 @@ class Volunteer(IndexedDocument):
          Raise exception when the phone number is invalid (length, no country code).
         """
 
-        def __init__(self, msg):
-            super().__init__(msg)
+        def __init__(self, phone):
+            super().__init__(f"Invalid phone number: {phone}.")
 
     class InvalidCampException(Exception):
         """
@@ -127,19 +127,3 @@ class Volunteer(IndexedDocument):
 
         def __init__(self, camp):
             super().__init__(f"Camp {camp} does not exist.")
-
-
-volunteerA = Volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin', phone='+012345', camp='UCL')
-# print(volunteerA)
-#
-# volunteerA.firstname = 'Yun-Tzu'
-# volunteerA.save()
-# print(volunteerA)
-
-# print(volunteer.all())
-# print(volunteer.find('yunsy'))
-# Volunteer.delete()
-
-# volunteerB = Volunteer.find('yunsy')
-# volunteerB.delete()
-# print(Volunteer.find('yunsy'))
