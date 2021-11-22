9908b8cf8a7f210fed7c67026c68a8cd1920db09
Dora <dorayin0602@gmail.com>
2021-11-22 15:25:54 +0000

    Saved volunteer2.py and test_volunteer2.py for release 2

--- models/tests/test_volunteer.py
+++ models/tests/test_volunteer.py
@@ -15,22 +15,22 @@ class TestVolunteer(unittest.TestCase):
     def test_invalid_firstname(self):
         with self.assertRaises(Volunteer.InvalidFirstnameException):
             Volunteer(username='yunsy', password='abc', firstname='Y', lastname="Yin",
-                                phone="+4477123456", camp=Camp("UCL"), availability=True)
+                      phone="+4477123456", camp=Camp("UCL"), availability=True)
 
     def test_invalid_lastname(self):
         with self.assertRaises(Volunteer.InvalidLastnameException):
             Volunteer(username='yunsy', password='abc', firstname='Yunsy', lastname="Y",
-                                phone="+4477123456", camp=Camp("UCL"), availability=True)
+                      phone="+4477123456", camp=Camp("UCL"), availability=True)
 
     def test_invalid_phone1(self):
         with self.assertRaises(Volunteer.InvalidPhoneException):
             Volunteer(username='yunsy', password='abc', firstname='Yunsy', lastname="Yin",
-                                phone="+123", camp=Camp("UCL"), availability=True)
+                      phone="+123", camp=Camp("UCL"), availability=True)
 
     def test_invalid_phone2(self):
         with self.assertRaises(Volunteer.InvalidPhoneException):
             Volunteer(username='yunsy', password='abc', firstname='Yunsy', lastname="Yin",
-                                phone="4477123456", camp=Camp("UCL"), availability=True)
+                      phone="4477123456", camp=Camp("UCL"), availability=True)
 
 
 if __name__ == '__main__':
