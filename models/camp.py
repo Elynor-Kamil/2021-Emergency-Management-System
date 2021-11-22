4c192a5bd2147ddbefae9eba83e44b282719277d
Dennis Yung <dennisylyung@gmail.com>
2021-11-22 15:34:03 +0000

    adapted camp and refugee, not tested

--- models/camp.py
+++ models/camp.py
@@ -1,9 +1,13 @@
 from models.base.document import Document
-from models.base.field import Field
+from models.base.field import Field, ReferenceDocumentsField
+from models.refugee import Refugee
+from models.volunteer import Volunteer
 
 
 class Camp(Document):
     name = Field(primary_key=True)
+    volunteers = ReferenceDocumentsField(data_type=Volunteer)
+    refugees = ReferenceDocumentsField(data_type=Refugee)
 
     def __str__(self):
         return self.name
