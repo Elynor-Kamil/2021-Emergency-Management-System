c3e6da90039adfa0b2d23110b7d6a78b175273bd
Michelle Chan <michelle.112348@gmail.com>
2021-11-26 22:11:07 +0000

    update plan_statistics with small tests

--- models/plan_statistics.py
+++ models/plan_statistics.py
@@ -1,8 +1,8 @@
 from models.refugee import Refugee
-from datetime import date
 from models.volunteer import Volunteer
 from models.camp import Camp
 from models.plan import Plan
+from datetime import date
 import math
 
 volunteer1 = Volunteer(username='yunsy',
@@ -77,8 +77,7 @@ plan = Plan(name='My Plan',
 
 def find_volunteers(Camp):
     """
-    Function to find the number of active volunteers at a camp.
-    If a volunteer is not active then they will not be included in the count.
+    Function to find the number of active volunteers at a camp. If a volunteer is not active then they will not be included in the count.
     """
     volunteer_count = 0
     for volunteer in Camp.volunteers:
@@ -88,8 +87,7 @@ def find_volunteers(Camp):
 
 def find_refugees(Camp):
     """
-    Function to find the number of refugees at a camp.
-    Includes the count of both the head of family and the family members in a single count.
+    Function to find the number of refugees at a camp. Includes the count of both the head of family and the family members in a single count.
     """
     refugee_count = 0
     for refugee in Camp.refugees:
@@ -97,6 +95,9 @@ def find_refugees(Camp):
     return refugee_count
 
 def plan_statistics_function(Plan):
+    """
+    Review the plan data for a specific plan and return each camp in a plan with total active volunteers and total refugees. Done as a dictionary.
+    """
     plan_statistics = {}
     for camp in Plan.camps:
         num_of_volunteers = find_volunteers(camp)
@@ -106,6 +107,9 @@ def plan_statistics_function(Plan):
     return plan_statistics
 
 def calculate_volunteer_needed(Camp, plan_statistics):
+    """
+    Calculate the extra volunteers needed and remaining volunteers not needed for a camp by 1:20 number of volunteers and number of refugees ratio.
+    """
     num_of_volunteers, num_of_refugees = plan_statistics[Camp.name]
     extra_volunteers_needed, remaining_volunteers = 0, 0
     ideal_volunteers_num = int(math.ceil(num_of_refugees / 20))
