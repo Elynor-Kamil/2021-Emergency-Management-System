21e7151ad7889afc48a349663d9cd63a2862a10f
Dora <dorayin0602@gmail.com>
2021-11-29 18:39:56 +0000

    updated the functions edit_volunteer_profile and change_volunteer_camp to 5 sub-functions according to the edited item.

--- controller/volunteer_controller.py
+++ controller/volunteer_controller.py
@@ -35,28 +35,53 @@ def view_volunteer_profile(volunteer: Volunteer) -> str:
     return str(volunteer)
 
 
-def edit_volunteer_profile(volunteer: Volunteer, firstname: str, lastname: str,
-                           phone: str) -> Volunteer:
-    """
-    Yunsy, Yingbo
-    A function used by admin and volunteer
-    Should include re-assign volunteer to a camp
-    """
-    # A new menu class EditVolunteerMenu was built to list the items to be edited in the new branch cli-edit-volunteer
-    # The function edit_volunteer_profile is divided into 5 sub-functions according to the edited item.
-    pass
+def edit_firstname(volunteer: Volunteer, firstname: str) -> Volunteer:
+    volunteer_called = volunteer
+    volunteer_called.firstname = firstname
+    volunteer_called.save()
+    return volunteer_called
 
 
-def change_volunteer_camp(volunteer: Volunteer, camp: Camp, is_admin: bool) -> Volunteer:
-    """
-    The admin role
-    :param is_admin: if this method is called by an admin
-    :param volunteer:
-    :param camp: the new camp
-    :return:
-    """
-    # A new menu class EditVolunteerMenu was built to list the items to be edited including camp in the new branch cli-edit-volunteer
-    pass
+def edit_lastname(volunteer: Volunteer, lastname: str) -> Volunteer:
+    volunteer_called = volunteer
+    volunteer_called.lastname = lastname
+    volunteer_called.save()
+    return volunteer_called
+
+
+def edit_phone(volunteer: Volunteer, phone: str) -> Volunteer:
+    volunteer_called = volunteer
+    volunteer_called.phone = phone
+    volunteer_called.save()
+    return volunteer_called
+
+
+def edit_camp(volunteer: Volunteer, camp: Camp, is_admin: bool) -> Volunteer:
+    volunteer_called = volunteer
+    old_camp = volunteer_called.camp
+    if is_admin:
+        old_camp.volunteers.remove(volunteer_called)
+        old_camp.save()
+        camp.volunteers.add(volunteer_called)
+        camp.save()
+        return volunteer_called
+    else:
+        if old_camp.plan() == camp.plan():
+            old_camp.volunteers.remove(volunteer_called)
+            old_camp.save()
+            camp.volunteers.add(volunteer_called)
+            camp.save()
+            return volunteer_called
+        else:
+            print("You can only select a camp under the same plan.")
+            return volunteer_called
+
+
+def edit_availability(volunteer: Volunteer, availability: str) -> Volunteer:
+    volunteer_called = volunteer
+    volunteer_called.availability = availability
+    volunteer_called.save()
+    return volunteer_called
 
 
 def deactivate_volunteer(volunteer: Volunteer) -> Volunteer:
