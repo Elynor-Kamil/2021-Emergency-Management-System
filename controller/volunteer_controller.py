bbd7de28a6e4203fd8896ff98c11c7c19a9766b5
Dora <dorayin0602@gmail.com>
2021-11-29 17:14:37 +0000

    relocated functions regarding editing volunteer profiles to the new branch cli-edit-volunteer

--- controller/volunteer_controller.py
+++ controller/volunteer_controller.py
@@ -23,6 +23,30 @@ def view_volunteer_profile(volunteer: Volunteer) -> str:
     return str(volunteer)
 
 
+def edit_volunteer_profile(volunteer: Volunteer, firstname: str, lastname: str,
+                           phone: str) -> Volunteer:
+    """
+    Yunsy, Yingbo
+    A function used by admin and volunteer
+    Should include re-assign volunteer to a camp
+    """
+    # A new menu class EditVolunteerMenu was built to list the items to be edited in the new branch cli-edit-volunteer
+    # The function edit_volunteer_profile is divided into 5 sub-functions according to the edited item.
+    pass
+
+
+def change_volunteer_camp(volunteer: Volunteer, camp: Camp, is_admin: bool) -> Volunteer:
+    """
+    The admin role
+    :param is_admin: if this method is called by an admin
+    :param volunteer:
+    :param camp: the new camp
+    :return:
+    """
+    # A new menu class EditVolunteerMenu was built to list the items to be edited including camp in the new branch cli-edit-volunteer
+    pass
+
+
 def deactivate_volunteer(volunteer: Volunteer) -> Volunteer:
     volunteer_called = volunteer
     volunteer_called.account_activated = False
