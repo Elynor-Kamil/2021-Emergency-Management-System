4b77bd3dfc577cf474f95e5b2fff0f967e16d6f5
Dora <dorayin0602@gmail.com>
2021-11-29 17:00:54 +0000

    relocated functions regarding editing volunteer profiles to the new branch cli-edit-volunteer

--- controller/volunteer_controller.py
+++ controller/volunteer_controller.py
@@ -23,72 +23,21 @@ def view_volunteer_profile(volunteer: Volunteer) -> str:
     return str(volunteer)
 
 
-def edit_firstname(username: str, firstname: str) -> Volunteer:
-    volunteer_called = find_volunteer(username)
-    volunteer_called.firstname = firstname
-    volunteer_called.save()
-    return volunteer_called
-
-
-def edit_lastname(username: str, lastname: str) -> Volunteer:
-    volunteer_called = find_volunteer(username)
-    volunteer_called.lastname = lastname
-    volunteer_called.save()
-    return volunteer_called
-
-
-def edit_phone(username: str, phone: str) -> Volunteer:
-    volunteer_called = find_volunteer(username)
-    volunteer_called.phone = phone
-    volunteer_called.save()
-    return volunteer_called
-
-
-def edit_camp(username: str, camp: Camp, is_admin: bool) -> Volunteer:
-    if is_admin:
-        volunteer_called = find_volunteer(username)
-        old_camp = volunteer_called.camp
-        old_camp.volunteers.remove(volunteer_called)
-        old_camp.save()
-        camp.volunteers.add(volunteer_called)
-        camp.save()
-        return volunteer_called
-    else:
-        volunteer_called = find_volunteer(username)
-        old_camp = volunteer_called.camp
-        if old_camp.plan() == camp.plan():
-            old_camp.volunteers.remove(volunteer_called)
-            old_camp.save()
-            camp.volunteers.add(volunteer_called)
-            camp.save()
-            return volunteer_called
-        else:
-            print("You can only select a camp under the same plan.")
-            return volunteer_called
-
-
-def edit_availability(username: str, availability: str) -> Volunteer:
-    volunteer_called = find_volunteer(username)
-    volunteer_called.availability = availability
-    volunteer_called.save()
-    return volunteer_called
-
-
-def deactivate_volunteer(username: str) -> Volunteer:
-    volunteer_called = find_volunteer(username)
+def deactivate_volunteer(volunteer: Volunteer) -> Volunteer:
+    volunteer_called = volunteer
     volunteer_called.account_activated = False
     volunteer_called.save()
     return volunteer_called
 
 
-def reactivate_volunteer(username: str) -> Volunteer:
-    volunteer_called = find_volunteer(username)
+def reactivate_volunteer(volunteer: Volunteer) -> Volunteer:
+    volunteer_called = volunteer
     volunteer_called.account_activated = True
     volunteer_called.save()
     return volunteer_called
 
 
-def delete_volunteer(username: str) -> None:
-    volunteer_called = find_volunteer(username)
+def delete_volunteer(volunteer: Volunteer) -> None:
+    volunteer_called = volunteer
     volunteer_called.camp = None
     volunteer_called.delete()
