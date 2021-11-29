4dbd13c11d1fe24e6e1fdfe93fe22881f68c6973
Yingbo Zhou <93536607+YingboZhou@users.noreply.github.com>
2021-11-29 23:14:23 +0000

    Update refugee controller.

--- controller/refugee_controller.py
+++ controller/refugee_controller.py
@@ -5,38 +5,55 @@ from models.camp import Camp
 from models.refugee import Refugee
 
 
-def create_refugee(firstname: str,
+def create_refugee(user_id: str,
+                   firstname: str,
                    lastname: str,
                    camp: Camp,
                    num_of_family_member: int,
                    starting_date: date,
                    medical_condition_type: Refugee.MedicalCondition) -> Refugee:
     """
-    Yingbo, Michelle
-    A function used by admin and volunteer.
+    Function to create and save refugee.
     """
-    pass
+    new_refugee = Refugee(user_id = user_id,
+                          firstname = firstname,
+                          lastname = lastname,
+                          num_of_family_member = num_of_family_member,
+                          starting_date = starting_date,
+                          medical_condition_type = medical_condition_type)
+    camp.refugees.add(new_refugee)
+    return new_refugee
 
 
 def find_refugee(refugee_id: int) -> Refugee:
     """
-    Yingbo, Michelle
-
+    Function to find if refugee exists and returns the refugee class profile. Return None if no such refugee exist.
     """
-    pass
+    for plan in Plan.all():
+        plan_name = str(plan.name)
+        curr_plan = Plan.find(plan_name)
+        for camp in curr_plan.camps:
+            for refugee in camp.refugees:
+                if refugee_id == refugee.user_id:
+                    return refugee
+    return None
 
 
 def view_refugee(refugee: Refugee) -> str:
     """
-    Yingbo, Michelle
     A function used by admin and volunteer.
     """
-    pass
+
+    # volunteer: 1. to view all refugees in same camp 2. to view one specific refugee in corresponding camp
+    # admin: 1. to view all refugees as a whole/by plan/by camps 2. view one specific refugee restriction/free
+
+    print(refugee)
 
 
 def delete_refugee(refugee: Refugee) -> None:
     """
-    Yingbo
     A function used by admin and volunteer.
     """
-    pass
+    refugee_called = refugee
+    refugee_called.camp = None
+    refugee_called.delete()
