7c89d51e55b1e5647160bc67e9383a585ef53d89
Shalaka Yerawadekar <shalakya@live.com>
2021-11-30 00:00:11 +0000

    test_plan - added tests regarding close-plan feature.

--- models/tests/test_plan.py
+++ models/tests/test_plan.py
@@ -107,6 +107,7 @@ class PlanTest(unittest.TestCase):
         self.assertEqual(date.today(), plan.start_date)
 
     # might need to put the following tests solely in test_plan_controller
+
     def test_plan_status(self):
         """
         Test status flag of plan. Should be false is plan is open.
