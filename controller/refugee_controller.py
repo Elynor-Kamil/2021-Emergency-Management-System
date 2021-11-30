f03f2b815c91ab51739dd3febacdf8d2eef2b3d8
Yingbo Zhou <93536607+YingboZhou@users.noreply.github.com>
2021-11-30 15:06:05 +0000

    Update create refugee in refugee controller

--- controller/refugee_controller.py
+++ controller/refugee_controller.py
@@ -1,36 +1,19 @@
 ###---- Manage Refugee Menu ----
 from datetime import date
+from enum import Enum
+from typing import Type
 
 from models.camp import Camp
 from models.refugee import Refugee
+from controller.controller_error import ControllerError
 
 
-def list_medical_condition_types() -> dict:
+def list_medical_condition_types() -> Type[Enum]:
     """
-    Dictionary of medical condition type.
+    List all options for medical condition type.
     :return:
     """
-    medical_condition_types = {'Cancer': Refugee.MedicalCondition.CANCER,
-                              'Chronic kidney disease': Refugee.MedicalCondition.CHRONICKIDNEY,
-                              'Chronic liver disease': Refugee.MedicalCondition.CHRONICLIVER,
-                              'Chronic lung diseases': Refugee.MedicalCondition.CHRONICLUNG,
-                              'Dementia or other neurological conditions': Refugee.MedicalCondition.DEMENTIA_OR_NEUROLOGICAL,
-                              'Diabetes': Refugee.MedicalCondition.DIABETES,
-                              'Down Syndrome': Refugee.MedicalCondition.DOWNSYNDROME,
-                              'Heart conditions': Refugee.MedicalCondition.HEART,
-                              'HIV infection': Refugee.MedicalCondition.HIV,
-                              'Immunocompromised state': Refugee.MedicalCondition.IMMUNOCOMPROMISED,
-                              'Mental health conditions': Refugee.MedicalCondition.MENTALHEALTH,
-                              'Overweight and obesity': Refugee.MedicalCondition.OVERWEIGHT_OR_OBESITY,
-                              'Pregnancy': Refugee.MedicalCondition.PREGNANCY,
-                              'Sickle cell or thalassemia': Refugee.MedicalCondition.SICKLECELL_OR_THALASSEMIA,
-                              'Smoking, current or former': Refugee.MedicalCondition.SMOKING,
-                              'Solid organ or blood stem cell transplant': Refugee.MedicalCondition.TRANSPLANT,
-                              'Stroke or cerebrovascular disease, which affects blood flow to the brain': Refugee.MedicalCondition.STROKE_OR_CEREBROVASCULAR,
-                              'Substance use disorders': Refugee.MedicalCondition.SUBSTANCE_USE,
-                              'Tuberculosis': Refugee.MedicalCondition.TUBERCULOSIS,
-                              'Others': Refugee.MedicalCondition.OTHERS}
-    return medical_condition_types
+    return Refugee.MedicalCondition()
 
 
 def create_refugee(user_id: str,
@@ -43,14 +26,21 @@ def create_refugee(user_id: str,
     """
     Function to create and save refugee.
     """
-    new_refugee = Refugee(user_id = user_id,
-                          firstname = firstname,
-                          lastname = lastname,
-                          num_of_family_member = num_of_family_member,
-                          starting_date = starting_date,
-                          medical_condition_type = medical_condition_type)
-    camp.refugees.add(new_refugee)
-    return new_refugee
+    try:
+        for plan in Plan.all():
+            plan_name = str(plan.name)
+            curr_plan = Plan.find(plan_name)
+            if camp == curr_plan.camps:
+                new_refugee = Refugee(user_id = user_id,
+                                      firstname = firstname,
+                                      lastname = lastname,
+                                      num_of_family_member = num_of_family_member,
+                                      starting_date = starting_date,
+                                      medical_condition_type = medical_condition_type)
+                camp.refugees.add(new_refugee)
+                return new_refugee
+    except:
+        ControllerError("Invalid refugee profile. Please try again.")
 
 
 def find_refugee(refugee_id: int) -> Refugee:
