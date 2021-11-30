8e8d741837e6a07900f5375dda58cd017c729c85
Yingbo Zhou <93536607+YingboZhou@users.noreply.github.com>
2021-11-30 12:14:07 +0000

    Update refugee controller.

--- controller/refugee_controller.py
+++ controller/refugee_controller.py
@@ -5,6 +5,34 @@ from models.camp import Camp
 from models.refugee import Refugee
 
 
+def list_medical_condition_types() -> dict:
+    """
+    Dictionary of medical condition type.
+    :return:
+    """
+    medical_condition_types = {'Cancer': Refugee.MedicalCondition.CANCER,
+                              'Chronic kidney disease': Refugee.MedicalCondition.CHRONICKIDNEY,
+                              'Chronic liver disease': Refugee.MedicalCondition.CHRONICLIVER,
+                              'Chronic lung diseases': Refugee.MedicalCondition.CHRONICLUNG,
+                              'Dementia or other neurological conditions': Refugee.MedicalCondition.DEMENTIA_OR_NEUROLOGICAL,
+                              'Diabetes': Refugee.MedicalCondition.DIABETES,
+                              'Down Syndrome': Refugee.MedicalCondition.DOWNSYNDROME,
+                              'Heart conditions': Refugee.MedicalCondition.HEART,
+                              'HIV infection': Refugee.MedicalCondition.HIV,
+                              'Immunocompromised state': Refugee.MedicalCondition.IMMUNOCOMPROMISED,
+                              'Mental health conditions': Refugee.MedicalCondition.MENTALHEALTH,
+                              'Overweight and obesity': Refugee.MedicalCondition.OVERWEIGHT_OR_OBESITY,
+                              'Pregnancy': Refugee.MedicalCondition.PREGNANCY,
+                              'Sickle cell or thalassemia': Refugee.MedicalCondition.SICKLECELL_OR_THALASSEMIA,
+                              'Smoking, current or former': Refugee.MedicalCondition.SMOKING,
+                              'Solid organ or blood stem cell transplant': Refugee.MedicalCondition.TRANSPLANT,
+                              'Stroke or cerebrovascular disease, which affects blood flow to the brain': Refugee.MedicalCondition.STROKE_OR_CEREBROVASCULAR,
+                              'Substance use disorders': Refugee.MedicalCondition.SUBSTANCE_USE,
+                              'Tuberculosis': Refugee.MedicalCondition.TUBERCULOSIS,
+                              'Others': Refugee.MedicalCondition.OTHERS}
+    return medical_condition_types
+
+
 def create_refugee(user_id: str,
                    firstname: str,
                    lastname: str,
@@ -47,13 +75,4 @@ def view_refugee(refugee: Refugee) -> str:
     # volunteer: 1. to view all refugees in same camp 2. to view one specific refugee in corresponding camp
     # admin: 1. to view all refugees as a whole/by plan/by camps 2. view one specific refugee restriction/free
 
-    print(refugee)
-
-
-def delete_refugee(refugee: Refugee) -> None:
-    """
-    A function used by admin and volunteer.
-    """
-    refugee_called = refugee
-    refugee_called.camp = None
-    refugee_called.delete()
+    return str(refugee)
