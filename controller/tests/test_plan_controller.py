a7d9508dcd2fbeac654d70acc9f8852cc7397caf
Shalaka Yerawadekar <shalakay@live.com>
2021-12-01 13:05:16 +0000

    Changes made according to PR.

--- controller/tests/test_plan_controller.py
+++ controller/tests/test_plan_controller.py
@@ -3,29 +3,12 @@ from datetime import date
 
 from models.camp import Camp
 from models.plan import Plan
-from controller.plan_controller import close_plan
-from controller.plan_controller import find_plan
-from controller.plan_controller import create_plan
-from controller.plan_controller import list_plans
-from controller.plan_controller import create_camps
-from controller.plan_controller import controller_error
+import controller.plan_controller as pc
 
 
 class PlanControllerTest(unittest.TestCase):
     def setUp(self):
         Plan.delete_all()
-
-    def test_plan_status(self):
-        """
-        Test status flag of plan. Should be false is plan is open.
-        """
-        plan = Plan(name='My Plan',
-                    emergency_type=Plan.EmergencyType.EARTHQUAKE,
-                    description='Test emergency plan',
-                    geographical_area='',
-                    camps=[Camp(name='TestCamp')])
-        self.assertEqual(False, plan.close_plan_status)
-        self.setUp()
 
     def test_close_plan_controller(self):
         """
@@ -36,23 +19,10 @@ class PlanControllerTest(unittest.TestCase):
                     description='Test emergency plan',
                     geographical_area='',
                     camps=[Camp(name='TestCamp')])
-        close_plan(plan)
-        self.assertEqual(True, plan.close_plan_status)
-        self.setUp()
+        pc.close_plan(plan)
+        self.assertEqual(True, plan.is_closed)
 
-    def test_no_close_date(self):
-        """
-        Test that close date is not set during plan creation.
-        """
-        plan = Plan(name='My Plan',
-                    emergency_type=Plan.EmergencyType.EARTHQUAKE,
-                    description='Test emergency plan',
-                    geographical_area='',
-                    camps=[Camp(name='TestCamp')])
-        self.assertEqual(None, plan.close_date)
-        self.setUp()
-
-    def test_close_date_set(self):
+    def test_initialised_plan_no_close_date(self):
         """
         Test plan closure date is set to today's date when plan is closed.
         """
@@ -62,7 +32,19 @@ class PlanControllerTest(unittest.TestCase):
                     description='Test emergency plan',
                     geographical_area='',
                     camps=[Camp(name='TestCamp')])
-        close_plan(plan)
+        self.assertEqual(None, plan.close_date)
+
+    def test_close_date_set_controller(self):
+        """
+        Test plan closure date is set to today's date when plan is closed.
+        """
+        self.setUp()
+        plan = Plan(name='My Plan',
+                    emergency_type=Plan.EmergencyType.EARTHQUAKE,
+                    description='Test emergency plan',
+                    geographical_area='',
+                    camps=[Camp(name='TestCamp')])
+        pc.close_plan(plan)
         self.assertEqual(date.today(), plan.close_date)
 
     def test_find_plan_controller(self):
@@ -74,50 +56,40 @@ class PlanControllerTest(unittest.TestCase):
                          description='Test emergency plan',
                          geographical_area='',
                          camps=[Camp(name='TestCamp')])
-        found_plan = find_plan('My Plan')
+        found_plan = pc.find_plan('My Plan')
         self.assertEqual(test_plan, found_plan)
-        self.setUp()
 
     def test_no_plan_found_controller(self):
         """
         Test that if no plan is found then an exception is raised.
         """
-        with self.assertRaises(controller_error.ControllerError):
-            find_plan('My Plan')
-
-        self.setUp()
-
-    def test_not_plan_instance(self):
-        """
-        Test that if input is not a Plan type then exception is raised.
-        """
-        camp_1 = Camp(name='TestCamp')
-        with self.assertRaises(controller_error.ControllerError):
-            find_plan(camp_1.name)
-
-        self.setUp()
+        with self.assertRaises(pc.controller_error.ControllerError):
+            pc.find_plan('My Plan')
 
     def test_create_plan_controller(self):
         """
         Test to see if plan is created correctly with controller.
         """
-        plan = create_plan(plan_name='My Plan',
+        plan = pc.create_plan(plan_name='My Plan',
+                              emergency_type=Plan.EmergencyType.EARTHQUAKE,
+                              description='Test emergency plan',
+                              geographical_area='',
+                              camps=[Camp(name='TestCamp')])
+        self.assertEqual('My Plan', plan.name)
+
+    def test_missing_camps_error_controller(self):
+        with self.assertRaises(pc.controller_error.ControllerError):
+            pc.create_plan(plan_name='My Plan',
                            emergency_type=Plan.EmergencyType.EARTHQUAKE,
                            description='Test emergency plan',
                            geographical_area='',
-                           camps=[Camp(name='TestCamp')])
-        self.assertEqual('My Plan', plan.name)
-        plan_camp = plan.camps.get('TestCamp')
-        self.assertListEqual([plan_camp.name], ['TestCamp'])
-        self.setUp()
+                           camps=[])
 
     def test_create_camps_controller(self):
         """
         Test that camps are added to plan correctly.
         """
-        create_camps('Camp 1')
-        self.assertIsInstance(create_camps('Camp 1'), Camp)
-        self.setUp()
+        self.assertIsInstance(pc.create_camps('Camp 1'), Camp)
 
     def test_list_plans_controller(self):
         """
@@ -141,7 +113,5 @@ class PlanControllerTest(unittest.TestCase):
              geographical_area='',
              camps=[Camp(name='TestCamp')])
 
-        find_list = [Plan.find('First Plan'), Plan.find('Second Plan'), Plan.find('Third Plan')]
-        list_plan = list_plans()
-        self.assertListEqual(find_list, list_plan)
-        self.setUp()
+        expected = [Plan.find('First Plan'), Plan.find('Second Plan'), Plan.find('Third Plan')]
+        self.assertListEqual(expected, pc.list_plans())
