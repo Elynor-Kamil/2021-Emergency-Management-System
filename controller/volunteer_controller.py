c87a69d2065fb7e0f90981db89594743e8e50d16
Dora <dorayin0602@gmail.com>
2021-12-02 12:19:12 +0000

    Merged main into the cli-edit-volunteer branch

--- controller/volunteer_controller.py
+++ controller/volunteer_controller.py
@@ -1,4 +1,3 @@
-import re
 from models.volunteer import Volunteer
 from models.camp import Camp
 from controller.controller_error import ControllerError
@@ -37,54 +36,50 @@ def view_volunteer_profile(volunteer: Volunteer) -> str:
 
 
 def edit_firstname(volunteer: Volunteer, firstname: str) -> Volunteer:
-    if len(firstname) > 1:
-        volunteer_called = volunteer
-        volunteer_called.firstname = firstname
-        volunteer_called.save()
-        return volunteer_called
-    else:
-        raise ControllerError(f"Invalid name: {firstname}. First name should be more than 1 character.")
+    try:
+        Volunteer.check_volunteer_firstname(firstname)
+        volunteer.firstname = firstname
+        volunteer.save()
+        return volunteer
+    except Volunteer.InvalidFirstnameException as e:
+        raise ControllerError(str(e))
 
 
 def edit_lastname(volunteer: Volunteer, lastname: str) -> Volunteer:
-    if len(lastname) > 1:
-        volunteer_called = volunteer
-        volunteer_called.lastname = lastname
-        volunteer_called.save()
-        return volunteer_called
-    else:
-        raise ControllerError(f"Invalid name: {lastname}. Last name should be more than 1 character.")
+    try:
+        Volunteer.check_volunteer_lastname(lastname)
+        volunteer.lastname = lastname
+        volunteer.save()
+        return volunteer
+    except Volunteer.InvalidLastnameException as e:
+        raise ControllerError(str(e))
 
 
 def edit_phone(volunteer: Volunteer, phone: str) -> Volunteer:
-    phone_text = r"\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]" \
-                 r"|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{5,14}$"
-    if re.search(phone_text, phone):
-        volunteer_called = volunteer
-        volunteer_called.phone = phone
-        volunteer_called.save()
-        return volunteer_called
-    else:
-        raise ControllerError(
-            f"Invalid phone number: {phone}. Phone number should start with a plus sign and international code")
+    try:
+        Volunteer.check_volunteer_phone(phone)
+        volunteer.phone = phone
+        volunteer.save()
+        return volunteer
+    except Volunteer.InvalidPhoneException as e:
+        raise ControllerError(str(e))
 
 
 def edit_camp(volunteer: Volunteer, camp: Camp, is_admin: bool) -> Volunteer:
-    volunteer_called = volunteer
-    old_camp = volunteer_called.camp
+    old_camp = volunteer.camp
     if is_admin:
-        old_camp.volunteers.remove(volunteer_called)
+        old_camp.volunteers.remove(volunteer)
         old_camp.save()
-        camp.volunteers.add(volunteer_called)
+        camp.volunteers.add(volunteer)
         camp.save()
-        return volunteer_called
+        return volunteer
     else:
-        if old_camp.plan() == camp.plan():
-            old_camp.volunteers.remove(volunteer_called)
+        if old_camp.plan == camp.plan:
+            old_camp.volunteers.remove(volunteer)
             old_camp.save()
-            camp.volunteers.add(volunteer_called)
+            camp.volunteers.add(volunteer)
             camp.save()
-            return volunteer_called
+            return volunteer
         else:
             raise ControllerError(f"Invalid camp: {camp}. You can only select a camp under the same plan.")
 
@@ -93,17 +88,15 @@ def edit_availability(volunteer: Volunteer, availability: str) -> Volunteer:
     list_true = ["True", "true", "T", "1"]
     list_false = ["False", "false", "F", "0"]
     if availability in list_true:
-        volunteer_called = volunteer
-        volunteer_called.availability = True
-        volunteer_called.save()
-        return volunteer_called
+        volunteer.availability = True
+        volunteer.save()
+        return volunteer
     elif availability in list_false:
-        volunteer_called = volunteer
-        volunteer_called.availability = False
-        volunteer_called.save()
-        return volunteer_called
+        volunteer.availability = False
+        volunteer.save()
+        return volunteer
     else:
-        raise ControllerError(f"Invalid input: {availability}. Only True or False is accepted.")
+        raise ControllerError(f"Invalid input: {availability}. Only 'True' or 'False' is accepted.")
 
 
 def deactivate_volunteer(volunteer: Volunteer) -> Volunteer:
