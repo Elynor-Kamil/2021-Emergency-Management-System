9497a2b4cf10b802454dd38e5199d71d9cdadc0b
Dora <dorayin0602@gmail.com>
2021-12-02 13:25:31 +0000

    Rebased to main

--- controller/volunteer_controller.py
+++ controller/volunteer_controller.py
@@ -59,6 +59,30 @@ def change_volunteer_camp(volunteer: Volunteer, camp: Camp, is_admin: bool) -> V
     pass
 
 
+def edit_volunteer_profile(volunteer: Volunteer, firstname: str, lastname: str,
+                           phone: str) -> Volunteer:
+    """
+    Yunsy, Yingbo
+    A function used by admin and volunteer
+    Should include re-assign volunteer to a camp
+    """
+    # A new menu class EditVolunteerMenu was built to list the items to be edited in the new branch cli-edit-volunteer
+    # The function edit_volunteer_profile is divided into 5 sub-functions according to the edited item.
+    pass
+
+
+def change_volunteer_camp(volunteer: Volunteer, camp: Camp, is_admin: bool) -> Volunteer:
+    """
+    The admin role
+    :param is_admin: if this method is called by an admin
+    :param volunteer:
+    :param camp: the new camp
+    :return:
+    """
+    # A new menu class EditVolunteerMenu was built to list the items to be edited including camp in the new branch cli-edit-volunteer
+    pass
+
+
 def deactivate_volunteer(volunteer: Volunteer) -> Volunteer:
     volunteer.account_activated = False
     volunteer.save()
