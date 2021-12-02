406934103e9635c339446c30ede6d513c9114b1a
Dora <dorayin0602@gmail.com>
2021-12-02 12:19:12 +0000

    Optimized codes in volunteer_controller.py, test_volunteer_controller.py and edit_volunteer_cli.py according to PR comments. Changed the check functions from private to public and separated the check_name function into check_firstname and check_lastname.

--- controller/tests/test_volunteer_controller.py
+++ controller/tests/test_volunteer_controller.py
@@ -7,52 +7,52 @@ from models.plan import Plan
 class TestVolunteerController(unittest.TestCase):
 
     def test_edit_firstname(self):
-        volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                               phone='+447519953189', camp=Camp(name='UCL'))
-        volunteer_edited = vc.edit_firstname(volunteer=volunteer_called, firstname='Yun-Tzu')
-        self.assertEqual(volunteer_edited.firstname, 'Yun-Tzu')
+        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = vc.edit_firstname(volunteer, 'Yun-Tzu')
+        self.assertEqual(volunteer.firstname, 'Yun-Tzu')
 
     def test_edit_firstname_invalid_input(self):
         with self.assertRaises(vc.ControllerError):
-            volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                                   phone='+447519953189', camp=Camp(name='UCL'))
-            vc.edit_firstname(volunteer=volunteer_called, firstname='Y')
+            volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                            phone='+447519953189', camp=Camp(name='UCL'))
+            vc.edit_firstname(volunteer=volunteer, firstname='Y')
 
     def test_edit_lastname(self):
-        volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                               phone='+447519953189', camp=Camp(name='UCL'))
-        volunteer_edited = vc.edit_lastname(volunteer=volunteer_called, lastname='Yinnn')
-        self.assertEqual(volunteer_edited.lastname, 'Yinnn')
+        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = vc.edit_lastname(volunteer=volunteer, lastname='Yinnn')
+        self.assertEqual(volunteer.lastname, 'Yinnn')
 
     def test_edit_lastname_invalid_input(self):
         with self.assertRaises(vc.ControllerError):
-            volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                                   phone='+447519953189', camp=Camp(name='UCL'))
-            vc.edit_lastname(volunteer=volunteer_called, lastname='Y')
+            volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                            phone='+447519953189', camp=Camp(name='UCL'))
+            vc.edit_lastname(volunteer=volunteer, lastname='Y')
 
     def test_edit_phone(self):
-        volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                               phone='+447519953189', camp=Camp(name='UCL'))
-        volunteer_edited = vc.edit_phone(volunteer=volunteer_called, phone='+441111111111')
-        self.assertEqual(volunteer_edited.phone, '+441111111111')
+        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = vc.edit_phone(volunteer=volunteer, phone='+441111111111')
+        self.assertEqual(volunteer.phone, '+441111111111')
 
     def test_edit_phone_without_international_code(self):
         with self.assertRaises(vc.ControllerError):
-            volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                                   phone='+447519953189', camp=Camp(name='UCL'))
-            vc.edit_phone(volunteer=volunteer_called, phone='441111111111')
+            volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                            phone='+447519953189', camp=Camp(name='UCL'))
+            vc.edit_phone(volunteer=volunteer, phone='441111111111')
 
     def test_edit_phone_too_short(self):
         with self.assertRaises(vc.ControllerError):
-            volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                                   phone='+447519953189', camp=Camp(name='UCL'))
-            vc.edit_phone(volunteer=volunteer_called, phone='+44123')
+            volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                            phone='+447519953189', camp=Camp(name='UCL'))
+            vc.edit_phone(volunteer=volunteer, phone='+44123')
 
     def test_edit_camp_by_admin(self):
-        volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                               phone='+447519953189', camp=Camp(name='UCL'))
-        volunteer_edited = vc.edit_camp(volunteer=volunteer_called, camp=Camp(name='Euston'), is_admin=True)
-        self.assertEqual(str(volunteer_edited.camp), 'Euston')
+        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = vc.edit_camp(volunteer=volunteer, camp=Camp(name='Euston'), is_admin=True)
+        self.assertEqual(str(volunteer.camp), 'Euston')
 
     def test_edit_camp_under_same_plan_by_volunteer(self):
         camp_1 = Camp(name='Camp 1')
@@ -62,10 +62,10 @@ class TestVolunteerController(unittest.TestCase):
              description='Test',
              geographical_area='',
              camps=[camp_1, camp_2])
-        volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                               phone='+447519953189', camp=camp_1)
-        volunteer_edited = vc.edit_camp(volunteer=volunteer_called, camp=camp_2, is_admin=False)
-        self.assertEqual(str(volunteer_edited.camp), 'Camp 2')
+        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                        phone='+447519953189', camp=camp_1)
+        volunteer = vc.edit_camp(volunteer=volunteer, camp=camp_2, is_admin=False)
+        self.assertEqual(str(volunteer.camp), 'Camp 2')
 
     def test_edit_camp_under_different_plan_by_volunteer(self):
         with self.assertRaises(vc.ControllerError):
@@ -81,28 +81,28 @@ class TestVolunteerController(unittest.TestCase):
                  description='Test',
                  geographical_area='',
                  camps=[camp_2])
-            volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                                   phone='+447519953189', camp=camp_1)
-            vc.edit_camp(volunteer=volunteer_called, camp=camp_2, is_admin=False)
+            volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                            phone='+447519953189', camp=camp_1)
+            vc.edit_camp(volunteer=volunteer, camp=camp_2, is_admin=False)
 
     def test_edit_availability_false(self):
-        volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                               phone='+447519953189', camp=Camp(name='UCL'))
-        volunteer_edited = vc.edit_availability(volunteer=volunteer_called, availability='False')
-        self.assertFalse(volunteer_edited.availability)
+        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = vc.edit_availability(volunteer=volunteer, availability='False')
+        self.assertFalse(volunteer.availability)
 
-    def test_edit_availability_True(self):
-        volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                               phone='+447519953189', camp=Camp(name='UCL'))
-        volunteer_edited = vc.edit_availability(volunteer=volunteer_called, availability='True')
-        self.assertTrue(volunteer_edited.availability)
+    def test_edit_availability_true(self):
+        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = vc.edit_availability(volunteer=volunteer, availability='True')
+        self.assertTrue(volunteer.availability)
 
     def test_edit_availability_invalid_input(self):
         with self.assertRaises(vc.ControllerError):
-            volunteer_called = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                                   phone='+447519953189', camp=Camp(name='UCL'))
-            volunteer_edited = vc.edit_availability(volunteer=volunteer_called, availability=False)
-            self.assertFalse(volunteer_edited.availability)
+            volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                                            phone='+447519953189', camp=Camp(name='UCL'))
+            volunteer = vc.edit_availability(volunteer=volunteer, availability=False)
+            self.assertFalse(volunteer.availability)
 
 
 if __name__ == '__main__':
