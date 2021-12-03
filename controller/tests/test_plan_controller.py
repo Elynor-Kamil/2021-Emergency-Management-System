da7664f762c45bff7623200cc21cb3696faf9a25
Michelle Chan <michelle.112348@gmail.com>
2021-12-03 16:11:03 +0000

    fix uniitests conflict

--- controller/tests/test_plan_controller.py
+++ controller/tests/test_plan_controller.py
@@ -165,32 +165,3 @@ class PlanControllerTest(unittest.TestCase):
                     camps=[camp])
         with self.assertRaises(ControllerError):
             pc.find_camp(plan, 'NonExistentCamp')
-
-    def test_view_plan_statistics_returns_str(self):
-        """
-        Test to confirm that view_plan_statistics function returns str
-        when it combines intermediate variables statistics and plan_info.
-        :return: str
-        """
-        Plan(name='test_plan1',
-             emergency_type=Plan.EmergencyType.EARTHQUAKE,
-             description='Test emergency plan',
-             geographical_area='London',
-             camps=[Camp(name='camp1')])
-        volunteer_a = Volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                phone='+447519953189')
-        volunteer_b = Volunteer(username='paul', password='root', firstname='Paul', lastname='Shoemaker',
-                                phone='+447519955439')
-        volunteer_c = Volunteer(username='gerald', password='root', firstname='Gerald', lastname='Smith',
-                                phone='+447511111111')
-        test_plan = Plan.find('test_plan1')
-        test_camp = test_plan.camps.get('camp1')
-        test_camp.volunteers.add(volunteer_a, volunteer_b, volunteer_c)
-        refugee1 = Refugee(firstname="Tom",
-                           lastname="Bond",
-                           num_of_family_member=600,
-                           starting_date=date(2020, 1, 2),
-                           medical_condition_type=[Refugee.MedicalCondition.HIV, Refugee.MedicalCondition.CANCER])
-        test_camp.refugees.add(refugee1)
-        test_view_plan_statistics = pc.view_plan_statistics(test_plan)
-        self.assertEqual(type(test_view_plan_statistics), str)
