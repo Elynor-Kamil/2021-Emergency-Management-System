d5b9c5a941a779588461091119a8d7d037242f5f
YunsyYin <64652077+YunsyYin@users.noreply.github.com>
2021-12-05 00:16:44 +0000

    Feature/cli edit volunteer (#34)
    * Updated the volunteer controller functions
    
    * Test
    
    * Updated EditVolunteerMenu class
    
    * added do_return function in class EditVolunteerMenu
    
    * updated codes
    
    * updated codes
    
    * created a new class "EditVolunteerMenu" under class ManageVolunteerMenu"
    
    * updated codes
    
    * relocated functions regarding editing volunteer profiles to the new branch cli-edit-volunteer
    
    * relocated functions regarding editing volunteer profiles to the new branch cli-edit-volunteer
    
    * relocated functions regarding editing volunteer profiles from the volunteer_controller branch to cli-edit-volunteer
    
    * relocated functions regarding editing volunteer profiles from the volunteer_controller branch to cli-edit-volunteer
    
    * updated the functions edit_volunteer_profile and change_volunteer_camp to 5 sub-functions according to the edited item.
    
    * Update controller signatures
    
    * add exception handling within the volunteer profile editing functions
    
    * write the unit test for volunteer_controller profile editing functions
    
    * Optimized codes in volunteer_controller.py, test_volunteer_controller.py and edit_volunteer_cli.py according to PR comments. Changed the check functions from private to public and separated the check_name function into check_firstname and check_lastname.
    
    * Changed the input type of the availability attribute from str to bool
    
    * Updated the unit tests based on the PR comments and the change of the input type of the availability attribute
    
    * Test
    
    * updated codes
    
    * relocated functions regarding editing volunteer profiles from the volunteer_controller branch to cli-edit-volunteer
    
    * updated the functions edit_volunteer_profile and change_volunteer_camp to 5 sub-functions according to the edited item.
    
    * add exception handling within the volunteer profile editing functions
    
    * Rebase to the main branch
    
    * Merged main into the cli-edit-volunteer branch
    
    * Changed the input type of the availability attribute from str to bool
    
    * Updated the unit tests based on the PR comments and the change of the input type of the availability attribute
    
    * Test
    
    * updated codes
    
    * relocated functions regarding editing volunteer profiles from the volunteer_controller branch to cli-edit-volunteer
    
    * rebase to main
    
    * Changed the input type of the availability attribute from str to bool
    
    * Updated def setUp() to create plan, camp and bvolunteer ahead in test_volunteer_controller.py
    
    * Completed all unit tests for all functions in volunteer_controller.py
    
    Co-authored-by: Dennis Yung <dennisylyung@gmail.com>

--- controller/tests/test_volunteer_controller.py
+++ controller/tests/test_volunteer_controller.py
@@ -1,49 +1,73 @@
 import unittest
 import controller.volunteer_controller as vc
-from models.volunteer import Volunteer
+from models.plan import Plan
 from models.camp import Camp
+from models.volunteer import Volunteer
 
 
 class TestVolunteerController(unittest.TestCase):
 
+    def setUp(self):
+        Volunteer.delete_all()
+        camp_1 = Camp(name='Camp-1')
+        camp_2 = Camp(name='Camp-2')
+        camp_3 = Camp(name='Camp-3')
+        Plan(name='Plan-1',
+             emergency_type=Plan.EmergencyType.EARTHQUAKE,
+             description='Test',
+             geographical_area='',
+             camps=[camp_1, camp_2])
+        Plan(name='Plan-2',
+             emergency_type=Plan.EmergencyType.EARTHQUAKE,
+             description='Test',
+             geographical_area='',
+             camps=[camp_3])
+        vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
+                            phone='+447519953189', camp=camp_1)
+
     def test_create_volunteer(self):
+        camp = Plan.find(key='Plan-1').camps.get('Camp-1')
         volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                        phone='+447519953189', camp=Camp(name='UCL'))
+                                        phone='+447519953189', camp=camp)
         self.assertIsInstance(volunteer, Volunteer)
 
     def test_create_volunteer_invalid_username(self):
+        camp = Plan.find(key='Plan-1').camps.get('Camp-1')
         with self.assertRaises(vc.ControllerError):
             vc.create_volunteer(username='yun', password='root', firstname='Yunsy', lastname='Yin',
-                                phone='+447519953189', camp=Camp(name='UCL'))
+                                phone='+447519953189', camp=camp)
 
     def test_create_volunteer_invalid_password(self):
+        camp = Plan.find(key='Plan-1').camps.get('Camp-1')
         with self.assertRaises(vc.ControllerError):
             vc.create_volunteer(username='yunsy', password='roo', firstname='Yunsy', lastname='Yin',
-                                phone='+447519953189', camp=Camp(name='UCL'))
+                                phone='+447519953189', camp=camp)
 
     def test_create_volunteer_invalid_firstname(self):
+        camp = Plan.find(key='Plan-1').camps.get('Camp-1')
         with self.assertRaises(vc.ControllerError):
             vc.create_volunteer(username='yunsy', password='roo', firstname='Y', lastname='Yin',
-                                phone='+447519953189', camp=Camp(name='UCL'))
+                                phone='+447519953189', camp=camp)
 
     def test_create_volunteer_invalid_lastname(self):
+        camp = Plan.find(key='Plan-1').camps.get('Camp-1')
         with self.assertRaises(vc.ControllerError):
             vc.create_volunteer(username='yunsy', password='roo', firstname='Yunsy', lastname='Y',
-                                phone='+447519953189', camp=Camp(name='UCL'))
+                                phone='+447519953189', camp=camp)
 
     def test_create_volunteer_invalid_phone_without_international_code(self):
+        camp = Plan.find(key='Plan-1').camps.get('Camp-1')
         with self.assertRaises(vc.ControllerError):
             vc.create_volunteer(username='yunsy', password='roo', firstname='Yunsy', lastname='Yin',
-                                phone='07519953189', camp=Camp(name='UCL'))
+                                phone='07519953189', camp=camp)
 
     def test_create_volunteer_invalid_phone_too_short(self):
+        camp = Plan.find(key='Plan-1').camps.get('Camp-1')
         with self.assertRaises(vc.ControllerError):
             vc.create_volunteer(username='yunsy', password='roo', firstname='Yunsy', lastname='Yin',
-                                phone='+44751', camp=Camp(name='UCL'))
+                                phone='+44751', camp=camp)
 
     def test_find_volunteer(self):
-        vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin', phone='+447519953189',
-                            camp=Camp(name='UCL'))
         volunteer = vc.find_volunteer('yunsy')
         self.assertIsInstance(volunteer, Volunteer)
 
@@ -52,28 +76,88 @@ class TestVolunteerController(unittest.TestCase):
             vc.find_volunteer('Yunsy')
 
     def test_view_volunteer_profile(self):
-        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = Volunteer.find('yunsy')
         volunteer_profile = vc.view_volunteer_profile(volunteer)
         self.assertIsInstance(volunteer_profile, str)
 
     def test_deactivate_volunteer(self):
-        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = Volunteer.find('yunsy')
         volunteer = vc.deactivate_volunteer(volunteer)
         self.assertFalse(volunteer.account_activated)
 
     def test_reactivate_volunteer(self):
-        volunteer = vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                        phone='+447519953189', camp=Camp(name='UCL'))
+        volunteer = Volunteer.find('yunsy')
         volunteer = vc.reactivate_volunteer(volunteer)
         self.assertTrue(volunteer.account_activated)
 
     def test_delete_volunteer(self):
+        volunteer = Volunteer.find('yunsy')
+        vc.delete_volunteer(volunteer)
         with self.assertRaises(vc.ControllerError):
-            vc.create_volunteer(username='yunsy', password='root', firstname='Yunsy', lastname='Yin',
-                                phone='+447519953189', camp=Camp(name='UCL'))
             vc.find_volunteer('Yunsy')
+
+    def test_edit_firstname(self):
+        volunteer = Volunteer.find('yunsy')
+        volunteer = vc.edit_firstname(volunteer, 'Yun-Tzu')
+        self.assertEqual(volunteer.firstname, 'Yun-Tzu')
+
+    def test_edit_firstname_invalid_input(self):
+        volunteer = Volunteer.find('yunsy')
+        with self.assertRaises(vc.ControllerError):
+            vc.edit_firstname(volunteer=volunteer, firstname='Y')
+
+    def test_edit_lastname(self):
+        volunteer = Volunteer.find('yunsy')
+        volunteer = vc.edit_lastname(volunteer=volunteer, lastname='Yinnn')
+        self.assertEqual(volunteer.lastname, 'Yinnn')
+
+    def test_edit_lastname_invalid_input(self):
+        volunteer = Volunteer.find('yunsy')
+        with self.assertRaises(vc.ControllerError):
+            vc.edit_lastname(volunteer=volunteer, lastname='Y')
+
+    def test_edit_phone(self):
+        volunteer = Volunteer.find('yunsy')
+        volunteer = vc.edit_phone(volunteer=volunteer, phone='+441111111111')
+        self.assertEqual(volunteer.phone, '+441111111111')
+
+    def test_edit_phone_without_international_code(self):
+        volunteer = Volunteer.find('yunsy')
+        with self.assertRaises(vc.ControllerError):
+            vc.edit_phone(volunteer=volunteer, phone='441111111111')
+
+    def test_edit_phone_too_short(self):
+        volunteer = Volunteer.find('yunsy')
+        with self.assertRaises(vc.ControllerError):
+            vc.edit_phone(volunteer=volunteer, phone='+44123')
+
+    def test_edit_camp_by_admin(self):
+        volunteer = Volunteer.find('yunsy')
+        camp = Plan.find(key='Plan-2').camps.get('Camp-3')
+        volunteer = vc.edit_camp(volunteer=volunteer, camp=camp, is_admin=True)
+        self.assertEqual(str(volunteer.camp), 'Camp-3')
+
+    def test_edit_camp_under_same_plan_by_volunteer(self):
+        volunteer = Volunteer.find('yunsy')
+        camp = Plan.find(key='Plan-1').camps.get('Camp-2')
+        volunteer = vc.edit_camp(volunteer=volunteer, camp=camp, is_admin=False)
+        self.assertEqual(str(volunteer.camp), 'Camp-2')
+
+    def test_edit_camp_under_different_plan_by_volunteer(self):
+        volunteer = Volunteer.find('yunsy')
+        camp = Plan.find(key='Plan-2').camps.get('Camp-3')
+        with self.assertRaises(vc.ControllerError):
+            vc.edit_camp(volunteer=volunteer, camp=camp, is_admin=False)
+
+    def test_edit_availability_false(self):
+        volunteer = Volunteer.find('yunsy')
+        volunteer = vc.edit_availability(volunteer=volunteer, availability=False)
+        self.assertFalse(volunteer.availability)
+
+    def test_edit_availability_true(self):
+        volunteer = Volunteer.find('yunsy')
+        volunteer = vc.edit_availability(volunteer=volunteer, availability=True)
+        self.assertTrue(volunteer.availability)
 
 
 if __name__ == '__main__':
