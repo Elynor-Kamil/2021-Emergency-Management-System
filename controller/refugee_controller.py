3e2a7889b6ce5d05d41a49fdc605949f92965861
Michelle Chan <92046833+michellechan1123@users.noreply.github.com>
2021-12-05 01:28:39 +0000

    Feature/controller refugee (#46)
    * Update refugee controller.
    
    * Update refugee controller.
    
    * Update create refugee in refugee controller
    
    * Update error handling for refugee controller.
    
    * Add ControllerError class and unit test for refugee controller.
    
    * Update error handling in the refugee_controller.
    
    * Update refugee_controller.py and test_refugee_controller.py
    
    * Update refugee_controller.py and test_refugee_controller.py
    
    * Update the create_refugee function in refugee_controller.py
    
    * update find refugee
    
    * Update refugee_controller.py and test_refugee_controller.py
    
    * update controller error message
    
    * Update the create_refugee function in refugee_controller.py
    
    * update controller error message
    
    * Update the list_medical_condition_types function in refugee_controller.py
    
    * update controller error message
    
    * fix controller error in find refugee
    
    * fix controller error in find refugee
    
    * tidy up code
    
    Co-authored-by: Yingbo Zhou <93536607+YingboZhou@users.noreply.github.com>
    Co-authored-by: Michelle Chan <michelle.112348@gmail.com>
    Co-authored-by: Dennis Yung <dennisylyung@gmail.com>

--- controller/refugee_controller.py
+++ controller/refugee_controller.py
@@ -1,9 +1,9 @@
-###---- Manage Refugee Menu ----
 from datetime import date
+from controller.controller_error import ControllerError
 from enum import Enum
-from typing import Type
-
+from typing import Type, Iterable
 from models.camp import Camp
+from models.plan import Plan
 from models.refugee import Refugee
 
 
@@ -11,7 +11,7 @@ def list_medical_condition_types() -> Type[Enum]:
     """
     List all options for medical condition type.
     """
-    pass
+    return Refugee.MedicalCondition
 
 
 def create_refugee(firstname: str,
@@ -19,33 +19,39 @@ def create_refugee(firstname: str,
                    camp: Camp,
                    num_of_family_member: int,
                    starting_date: date,
-                   medical_condition_type: Refugee.MedicalCondition) -> Refugee:
+                   medical_condition_type: Iterable[Refugee.MedicalCondition]) -> Refugee:
     """
-    Yingbo, Michelle
-    A function used by admin and volunteer.
+    Function to create and save refugee data.
     """
-    pass
+    try:
+        new_refugee = Refugee(firstname=firstname,
+                              lastname=lastname,
+                              num_of_family_member=num_of_family_member,
+                              starting_date=starting_date,
+                              medical_condition_type=medical_condition_type)
+        camp.refugees.add(new_refugee)
+        return new_refugee
+    except (Refugee.InvalidNameException,
+            Refugee.InvalidNumOfFamilyMemberException,
+            Refugee.InvalidStartingDateException,
+            Refugee.InvalidCampException) as e:
+        raise ControllerError(str(e))
 
 
 def find_refugee(refugee_id: int) -> Refugee:
     """
-    Yingbo, Michelle
-
+    Function to find if refugee exists and returns the refugee class profile. Raise error if no such refugee, camp and plan exist .
     """
-    pass
+    for plan in Plan.all():
+        for camp in plan.camps:
+            refugee = camp.refugees.get(refugee_id)
+            if refugee:
+                return refugee
+    raise ControllerError(f"Invalid refugee_id: {refugee_id}. The refugee is not found.")
 
 
 def view_refugee(refugee: Refugee) -> str:
     """
-    Yingbo, Michelle
-    A function used by admin and volunteer.
+    Function to be used by admin and volunteer to return refugee specified as a string.
     """
-    pass
-
-
-def delete_refugee(refugee: Refugee) -> None:
-    """
-    Yingbo
-    A function used by admin and volunteer.
-    """
-    pass
+    return str(refugee)
---
+++ controller/tests/test_refugee_controller.py
@@ -0,0 +1,85 @@
+import unittest
+from datetime import date
+from models.camp import Camp
+from models.plan import Plan
+from models.refugee import Refugee
+import controller.refugee_controller as rc
+
+
+class RefugeeControllerTest(unittest.TestCase):
+    """
+    Class for testing cases in create refugee function.
+    """
+
+    def test_create_refugee(self):
+        """
+        Test Refugee creation
+        """
+        test_camp = Camp(name='camp1')
+        Plan(name='test_plan',
+             emergency_type=Plan.EmergencyType.EARTHQUAKE,
+             description='Test emergency plan',
+             geographical_area='London',
+             camps=[test_camp])
+        new_refugee = rc.create_refugee("James", "Bond", test_camp, 6, date(2020, 1, 1),
+                                        [Refugee.MedicalCondition.HIV, Refugee.MedicalCondition.CANCER])
+        self.assertIsInstance(new_refugee, Refugee)
+
+    def test_find_and_return_refugee(self):
+        """
+        Test case where refugee specified is found in the file.
+        """
+        test_camp = Camp(name='camp1')
+        Plan(name='test_plan',
+             emergency_type=Plan.EmergencyType.EARTHQUAKE,
+             description='Test emergency plan',
+             geographical_area='London',
+             camps=[test_camp])
+        refugee = Refugee(firstname="Tom",
+                          lastname="Bond",
+                          num_of_family_member=600,
+                          starting_date=date(2020, 1, 2),
+                          medical_condition_type=[Refugee.MedicalCondition.HIV, Refugee.MedicalCondition.CANCER])
+        test_camp.refugees.add(refugee)
+        refugee_id = refugee.user_id
+        retrieved_refugee = rc.find_refugee(refugee_id)
+        self.assertEqual(refugee, retrieved_refugee)
+
+    def test_find_nonexistent_refugee(self):
+        """
+        Test case where refugee specified is not found in the file.
+        """
+        test_camp = Camp(name='camp1')
+        Plan(name='test_plan1',
+             emergency_type=Plan.EmergencyType.EARTHQUAKE,
+             description='Test emergency plan',
+             geographical_area='London',
+             camps=[test_camp])
+        refugee1 = Refugee(firstname="Tom",
+                           lastname="Bond",
+                           num_of_family_member=600,
+                           starting_date=date(2020, 1, 2),
+                           medical_condition_type=[Refugee.MedicalCondition.HIV, Refugee.MedicalCondition.CANCER])
+        test_camp.refugees.add(refugee1)
+        refugee_user_id = 1111111111
+        with self.assertRaises(rc.ControllerError):
+            rc.find_refugee(refugee_user_id)
+
+    def test_view_refugee_return_value(self):
+        """
+        Test case where refugee view is same as the refugee specified.
+        """
+        test_camp = Camp(name='camp1')
+        Plan(name='test_plan',
+             emergency_type=Plan.EmergencyType.EARTHQUAKE,
+             description='Test emergency plan',
+             geographical_area='London',
+             camps=[test_camp])
+        refugee = Refugee(firstname="Terry",
+                          lastname="Bimble",
+                          num_of_family_member=2,
+                          starting_date=date(2020, 1, 2),
+                          medical_condition_type=[Refugee.MedicalCondition.HIV, Refugee.MedicalCondition.CANCER])
+        test_camp.refugees.add(refugee)
+        refugee_str = rc.view_refugee(refugee)
+        self.assertEqual(refugee_str, str(refugee))
