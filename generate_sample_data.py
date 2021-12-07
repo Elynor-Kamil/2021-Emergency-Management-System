f72f945de7375ee6d6486aa4b8d5d4df87f83a6d
Dennis Yung <dennisylyung@gmail.com>
2021-12-07 14:40:00 +0000

    restore sample data

--- generate_sample_data.py
+++ generate_sample_data.py
@@ -18,39 +18,39 @@ if __name__ == '__main__':
 
     Admin.configure_initial_user()
 
-    # camps1 = [
-    #     Camp(name='camp1'),
-    #     Camp(name='camp2'),
-    #     Camp(name='camp3')
-    # ]
-    #
-    # camps2 = [
-    #     Camp(name='camp4'),
-    #     Camp(name='camp5')
-    # ]
-    #
-    # plan1 = Plan(name='plan1', emergency_type=Plan.EmergencyType.FIRE,
-    #              description='This is Plan 1', geographical_area='World', camps=camps1)
-    #
-    # plan2 = Plan(name='plan2', emergency_type=Plan.EmergencyType.FLOOD,
-    #              description='This is Plan 2', geographical_area='UK', camps=camps2)
-    #
-    # v1 = Volunteer(username='vvv1', password='11111', firstname='Volunteer', lastname='One', phone='+44123456789')
-    # v2 = Volunteer(username='vvv2', password='22222', firstname='Volunteer', lastname='Two', phone='+44123456789',
-    #                availability=False)
-    # v3 = Volunteer(username='vvv3', password='33333', firstname='Volunteer', lastname='Three', phone='+44123456789',
-    #                account_activated=False)
-    #
-    # camps1[0].volunteers.add(v1)
-    # camps1[1].volunteers.add(v2)
-    # camps2[0].volunteers.add(v3)
-    #
-    # r1 = Refugee(firstname='Refugee', lastname='One', num_of_family_member=4, starting_date=date(2021, 5, 4))
-    # camps1[0].refugees.add(r1)
-    # r2 = Refugee(firstname='Refugee', lastname='Two', num_of_family_member=1, starting_date=date(2020, 7, 16),
-    #              medical_condition_type=[Refugee.MedicalCondition.HIV, Refugee.MedicalCondition.DIABETES])
-    # camps1[0].refugees.add(r2)
-    # print(f'Refugee 1 id: {r1.user_id}')
-    # print(f'Refugee 2 id: {r2.user_id}')
+    camps1 = [
+        Camp(name='camp1'),
+        Camp(name='camp2'),
+        Camp(name='camp3')
+    ]
+
+    camps2 = [
+        Camp(name='camp4'),
+        Camp(name='camp5')
+    ]
+
+    plan1 = Plan(name='plan1', emergency_type=Plan.EmergencyType.FIRE,
+                 description='This is Plan 1', geographical_area='World', camps=camps1)
+
+    plan2 = Plan(name='plan2', emergency_type=Plan.EmergencyType.FLOOD,
+                 description='This is Plan 2', geographical_area='UK', camps=camps2)
+
+    v1 = Volunteer(username='vvv1', password='11111', firstname='Volunteer', lastname='One', phone='+44123456789')
+    v2 = Volunteer(username='vvv2', password='22222', firstname='Volunteer', lastname='Two', phone='+44123456789',
+                   availability=False)
+    v3 = Volunteer(username='vvv3', password='33333', firstname='Volunteer', lastname='Three', phone='+44123456789',
+                   account_activated=False)
+
+    camps1[0].volunteers.add(v1)
+    camps1[1].volunteers.add(v2)
+    camps2[0].volunteers.add(v3)
+
+    r1 = Refugee(firstname='Refugee', lastname='One', num_of_family_member=4, starting_date=date(2021, 5, 4))
+    camps1[0].refugees.add(r1)
+    r2 = Refugee(firstname='Refugee', lastname='Two', num_of_family_member=1, starting_date=date(2020, 7, 16),
+                 medical_condition_type=[Refugee.MedicalCondition.HIV, Refugee.MedicalCondition.DIABETES])
+    camps1[0].refugees.add(r2)
+    print(f'Refugee 1 id: {r1.user_id}')
+    print(f'Refugee 2 id: {r2.user_id}')
 
     print('Sample data generated successfully')
